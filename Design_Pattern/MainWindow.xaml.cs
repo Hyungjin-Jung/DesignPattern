696201f3f27bfa773ebcdcf1707a65c4f0a94224
Hyungjin <2hyungjin3@gmail.com>
2024-01-23 22:39:50 +0900

    초기 커밋.

---
+++ Design_Pattern/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Design_Pattern
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private static MainWindow _instance = null; // Singleton 인스턴스를 저장할 필드
+        private static readonly object padlock = new object();  // thread-safe를 위한 lock 객체
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static MainWindow Instance    // Singleton 인스턴스에 접근할 수 있는 public static 속성
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (_instance == null)  // 인스턴스가 아직 생성되지 않았다면 생성
+                    {
+                        _instance = new MainWindow();
+                    }
+                    return _instance; // 이미 생성된 Singleton 인스턴스를 반환
+                }
+            }
+        }
+    }
+}
---
+++ Design_Pattern/Observer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;  // 속성 변경 알림 이벤트
+
+        protected virtual void OnPropertyChanged(string propertyName)  // 속성 변경 알림을 발생시키는 메서드
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MyModel : ObservableObject
+    {
+        private string _myProperty;  // private 필드
+
+        public string MyProperty  // public 속성
+        {
+            get { return _myProperty; }
+            set
+            {
+                if (_myProperty != value)  // 값이 변경되었을 때만
+                {
+                    _myProperty = value;  // 필드 값을 업데이트하고
+                    OnPropertyChanged("MyProperty");  // 속성 변경 알림을 발생
+                }
+            }
+        }
+    }
+}
